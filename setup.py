6af8b48319a7fad0201a1441239a07cd67d21844
williamaherman <bherman2424@gmail.com>
2020-11-03 11:23:31 -0500

    feature: added setup page

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='Calculator',
       version='0.0.1',
-      description='Its a calculator',
+      description='It is a calculator',
       author='',
       author_email='',
       url='',
