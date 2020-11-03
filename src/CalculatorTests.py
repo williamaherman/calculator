b6bd70411c9d0a947fc181aa606b61d7da1ad2bf
williamaherman <bherman2424@gmail.com>
2020-11-03 17:29:20 -0500

    feature: added unit test

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,24 +1,26 @@
 import unittest
 from Calculator import Calculator
 from CsvReader import CsvReader
-from pprint import pprint
 
 
 class MyTestCase(unittest.TestCase):
+
     def setUp(self) -> None:
         self.calculator = Calculator()
+        testData = CsvReader('UnitTestAddition.csv').data
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
-    def test_subtraction(self):
-        test_data = CsvReader('/src/Unit Test Subtraction.csv').data
-        pprint(test_data)
-        for row in test_data:
-            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
-            self.assertEqual(self.calculator.result, int(row['Result']))
+    def test_results_property_calculator(self):
+        self.assertEqual(self.calculator.result, 0)
 
-    def test_results_property(self):
+    def test_add_method_calculator(self):
+        self.assertEqual(self.calculator.add(2, 2), 4)
+        self.assertEqual(self.calculator.result, 4)
+
+    def test_subtract_method_calculator(self):
+        self.assertEqual(self.calculator.subtract(2, 2), 0)
         self.assertEqual(self.calculator.result, 0)
 
 
