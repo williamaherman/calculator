c6fbe2273e08ca3544c5af520a6058251c267395
williamaherman <bherman2424@gmail.com>
2020-11-06 15:16:51 -0500

    feature: fixed calculator paths.

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,8 +1,13 @@
-from Calculator.Subtraction import subtraction
 from Calculator.Addition import addition
+from Calculator.Subtraction import subtraction
+from Calculator.Multiply import multiplication
+from Calculator.Division import division
+from Calculator.Square import square
+from Calculator.SquareRoot import square_root
 
 
 class Calculator:
+
     result = 0
 
     def __init__(self):
@@ -15,3 +20,19 @@ class Calculator:
     def subtract(self, a, b):
         self.result = subtraction(a, b)
         return self.result
+
+    def multiply(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+
+    def divide(self, divisor, dividend):
+        self.result = division(divisor, dividend)
+        return self.result
+
+    def square(self, a):
+        self.result = square(a)
+        return self.result
+
+    def square_root(self, a):
+        self.result = square_root(a);
+        return self.result;
